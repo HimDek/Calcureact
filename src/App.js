177b1d6478acf1eab4b8fc44bbbd0dac2a8ff797
Himprakash Deka <himprakashd@gmail.com>
2024-07-01 08:52:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
   const numClickHandler = (e) => {
     e.preventDefault();
     const value = e.target.innerHTML;
-    const num = calc.num.toString() + value;
+    const num = calc.num.toString().includes(".") && ["0", "."].includes(calc.num.toString()[-1]) ? calc.num.toString() + value : Number(calc.num.toString() + value);
     setState({
       ...calc,
       num: num,
